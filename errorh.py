3b3c7374669e981f03b2b087334e4cf728342e25
Amin Khair <aminomer@hotmail.com>
2020-05-10 18:22:51 -0400

    commit

---
+++ errorh.py
@@ -0,0 +1,16 @@
+# Import modules
+from __future__ import print_function
+while True:
+   try:
+      print("Let us solve the equation (x/2) / (x-y) ")
+      x = float(input("Please enter a value for x: "))
+      y = float(input("Please enter a value for y: "))
+
+      if x==0 or y==0:
+         break
+
+      z = (x/2) / (x-y)
+
+      print("Solving (x/2) / (x-y) for values x=", x, "and y=", y, "we get the result:", z)
+   except Exception as e:
+      print("There was an error with the code",str(e))
