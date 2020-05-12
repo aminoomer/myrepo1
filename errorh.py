f29c9a02b753d966a58672142e203a8644ec11ed
aminoomer <37510479+aminoomer@users.noreply.github.com>
2020-05-12 15:16:31 -0400

    update_1 errorh.py
    added a line to print 'This is the end of the file' to mark end of file.

--- errorh.py
+++ errorh.py
@@ -1,7 +1,7 @@
 # Import modules
 from __future__ import print_function
-while True:
-   try:
+   while True:
+      try:
       print("Let us solve the equation (x/2) / (x-y) ")
       x = float(input("Please enter a value for x: "))
       y = float(input("Please enter a value for y: "))
@@ -12,5 +12,6 @@ while True:
       z = (x/2) / (x-y)
 
       print("Solving (x/2) / (x-y) for values x=", x, "and y=", y, "we get the result:", z)
-   except Exception as e:
+      except Exception as e:
       print("There was an error with the code",str(e))
+ print('This is the end of the file')
